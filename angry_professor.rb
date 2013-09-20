aa8e3b739ac753e933c92ef5b3d4eb1b08b8dec8
Clayton Smith <csmith1@cs.uml.edu>
2013-09-20 10:42:45 -0400

    it now detects compile time failures, runs as desired

--- angry_professor.rb
+++ angry_professor.rb
@@ -10,10 +10,12 @@ c_failure = Array.new
 r_failure = Array.new 
 successful = Array.new
 
+inputs = ARGV
+
 command = "./a.out"
 
-for item in ARGV
-  inputs.push(item)
+for input in inputs
+  command = command + " " + input
 end
 
 ARGV.clear
@@ -32,34 +34,31 @@ for archive in archives
 
   puts username[0..-3] 
 
-  c_output = `gcc -Wall -ansi -pedantic #{username}`
+  c_output = `gcc -Werror -ansi -pedantic #{username}`
   c_result = $?.success?
   
   puts c_result
 
   if c_result == false 
     c_failure.push(username[0..-3])
-    
-    puts "%d " % c_result
+
+    puts c_result
     print "!!!ERROR WITH COMPOLATION!!!\n "
 
     next
   end
 
-  for input in inputs
-    command = command + " " + input
-  end
-
+  puts command
   r_output = `#{command}`
-  
-  if r_output.include? "Right turn" or r_output.include? "Right Turn" 
+  puts r_output + "\nCorrect?"
+
+  if r_output.upcase.include?("RIGHT TURN") 
     successful.push(username[0..-3])
     puts "In the bag"
     next
   end    
   
   
-  puts r_output + "\nCorrect?"
   
   if gets.chomp() != 'y'
   
@@ -73,9 +72,7 @@ for archive in archives
 end
 
 print "These are the compile-time failures: #{c_failure}\n"
-
 print "These are the run-time failures: #{r_failure}\n"
-
 print "These are the successes: #{successful}\n"
 
 
