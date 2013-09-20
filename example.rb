c5ae485a99a284ced58cfc18718d616d672e142d
Ian McGaunn <imcgaunn@cs.uml.edu>
2013-09-19 20:02:06 -0400

    a commit

--- example.rb
+++ example.rb
@@ -41,25 +41,9 @@ for archive in archives
   puts r_output
   puts "does this look correct?"
   if gets.chomp() == 'y'
-    # save it to a file of good programs
-   
-      
+    successful.push(username[0..-3])
+
 end
 
 print "these are the failures: #{c_failure}\n"
-
-## for every archive
-## untar archive, rename file to username.c
-## compile username.c to a.out
-## test output from a.out with answer from solutions
-
-# grab all inputs from ARGV
-
-for input in inputs
-  print "input: #{input}\n"
-end
-
-puts "archives: #{archives}"
-
-# get expected output from a solutions file
-expected_output = File.new("solution", "r")
+print "these are the successes: #{succesful}\n"
